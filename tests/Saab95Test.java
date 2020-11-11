d936ed44dc49e62b368ec604ef6a88740026a882
Oscar Palmqvist <oscar@palmqvist.eu>
2020-11-11 16:27:09 +0100

    testComplete

--- tests/Saab95Test.java
+++ tests/Saab95Test.java
@@ -1,5 +1,29 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class Saab95Test {
+    Saab95 testSaab;
+
+    @BeforeEach
+    public void init(){
+        testSaab = new Saab95();
+    }
+
+    @Test
+    public void breakDecreasingSpeed(){
+        testSaab.currentSpeed = 20;
+        double preSpeed = testSaab.currentSpeed;
+        testSaab.brake(0.5);
+        double postSpeed = testSaab.currentSpeed;
+
+        assertTrue(postSpeed < preSpeed);
+
+    }
+
+
+
+
 
 }
