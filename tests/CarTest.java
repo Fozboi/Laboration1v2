70e9893940ea8989da810307f10e9b6bf4a9300b
Fabian Flaa <fabbejunior@hotmail.com>
2020-11-12 10:35:53 +0100

    carTestsComplete

--- tests/CarTest.java
+++ tests/CarTest.java
@@ -1,5 +1,7 @@
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 import java.awt.*;
 
@@ -35,10 +37,47 @@ class CarTest {
         assertTrue(testCar.dir==1);
 
     }
+
     @Test
     public void turnLeftWorks(){
         testCar.turnLeft();
         assertTrue(testCar.dir==3);
 
+    }
+
+    @Test
+    public void breakDecreasesSpeed(){
+        testCar.currentSpeed = 20;
+        double preSpeed = testCar.currentSpeed;
+        testCar.brake(0.5);
+        double postSpeed = testCar.currentSpeed;
+
+        assertTrue(postSpeed < preSpeed);
+    }
+
+    @Test
+    public void gasIncreasesSpeed(){
+        testCar.currentSpeed = 20;
+        double preSpeed = testCar.currentSpeed;
+        testCar.gas(0.5);
+        double postSpeed = testCar.currentSpeed;
+
+        assertTrue(postSpeed > preSpeed);
+    }
+
+    @Test
+    public void brakeOnlyAcceptsValidValues(){
+        try{
+            testCar.brake(1.5);
+            fail("should have thrown exception but it didn't");
+        } catch (IllegalArgumentException expectedException){ }
+    }
+
+    @Test
+    public void gasOnlyAcceptsValidValues(){
+        try{
+            testCar.gas(-1.5);
+            fail("should have thrown exception but it didn't");
+        } catch (IllegalArgumentException expectedException){ }
     }
 }
