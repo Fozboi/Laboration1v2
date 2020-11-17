a4c54c1084172cbd30629d573a037bd690fa25b0
Fabian Flaa <fabbejunior@hotmail.com>
2020-11-17 12:38:48 +0100

    dadwd

--- src/Scania.java
+++ src/Scania.java
@@ -1,7 +1,15 @@
+import java.awt.*;
+
 public class Scania extends Car {
     private double trailerAngle;
 
-
+    public Scania(){
+        setNrDoors(2);
+        setColor(Color.white);
+        setEnginePower(90);
+        setModelName("Scania");
+        stopEngine();
+    }
 
     double speedFactor(){
         if(trailerAngle == 0){
