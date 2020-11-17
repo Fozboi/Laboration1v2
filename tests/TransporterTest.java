d4b5b584a943271ce1d3fbd9d6c210d4968c808b
Fabian Flaa <fabbejunior@hotmail.com>
2020-11-17 15:26:15 +0100

    dadwad

--- tests/TransporterTest.java
+++ tests/TransporterTest.java
@@ -13,17 +13,15 @@ class TransporterTest {
     @BeforeEach
     public void init(){
         testCar = new Saab95();
-        testTruck = new Transporter(2);
+        testTruck = new Transporter(1);
     }
 
     @Test
     public void canLoadCarWorks(){
-        Transporter testTruck2 = new Transporter(2);
-        Car testCar2 = new Saab95();
-        Car testCar3 = new Volvo240();
+        Transporter testTruck2 = new Transporter(1);
+        Car testCar2 = new Volvo240();
 
         testTruck.setRampDown();
-
         Point pos1 = new Point(100,100);
         Point pos2 = new Point(109,109);
         testTruck.setPosition(pos1);
@@ -38,10 +36,19 @@ class TransporterTest {
         testTruck.setRampDown();
 
         testCar2.setPosition(pos2);
-        testCar3.setPosition(pos2);
         testTruck.loadCar(testCar);
-        testTruck.loadCar(testCar2);
-        assertFalse(testTruck.canLoadCar(testCar3)); //transporter can't load car when 2 others are already on it
+        assertFalse(testTruck.canLoadCar(testCar2)); //transporter can't load car when 2 others are already on it
 
+    }
+
+    @Test
+    public void loadCarWorks(){
+        testTruck.setRampDown();
+        Point pos1 = new Point(100,100);
+        Point pos2 = new Point(109,109);
+        testTruck.setPosition(pos1);
+        testCar.setPosition(pos2);
+        testTruck.loadCar(testCar);
+        assertTrue(); //car was loaded and therefore added to the list of loaded cars
     }
 }
